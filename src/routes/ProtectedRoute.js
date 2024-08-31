da98b8a0d1a34f661455e286446ae31061a1e8cf
Satyam Kumar <56245953+satya4498@users.noreply.github.com>
2024-08-31 23:00:50 +0530

    Create ProtectedRoute.js

---
+++ src/routes/ProtectedRoute.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useAuth} from '../api/AuthContext'
+import Login from "../Pages/Login"
+import Cookies from 'js-cookie'
+
+const ProtectedRoute = ({Component,path}) => {
+    const {isAuthenticated } = useAuth();
+    const user = Cookies.get('user')
+  return (<>{(isAuthenticated || user)?<Component path={path}/>:<Login path={path}/>}</>)
+}
+
+export default ProtectedRoute
