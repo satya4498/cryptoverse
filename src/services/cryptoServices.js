0c7cadb50098c8f4ea47b0a5f957756bf79c4103
Satyam Kumar <56245953+satya4498@users.noreply.github.com>
2024-08-31 23:03:32 +0530

    Create cryptoServices.js

---
+++ src/services/cryptoServices.js
@@ -0,0 +1,125 @@
+import axios from "axios";
+import { createApi,fetchBaseQuery, } from "@reduxjs/toolkit/query/react";
+
+ const cryptoApiHeaders = {
+            'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+            'x-rapidapi-key': '3881f835b7mshcdc185d253e66b4p1381b6jsn94eb41fcf016'
+        }
+  const   params = {
+            referenceCurrencyUuid: 'yhjMzLPhuIDl'
+          }
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+const createRequest = (url)=> ({url,headers:cryptoApiHeaders, params})
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptoData: builder.query({
+            query: ()=> createRequest('/stats')
+        }),
+        // getCryptoDetils: builder.query({
+        //     query: '/exchanges',
+        //     headers: cryptoApiHeaders
+        // })
+    
+})
+})
+export const {useGetCryptoDataQuery} = cryptoApi
+
+export const getCryptoDetils = async (params) => {
+    try {
+        const response = await axios.get('https://api.coingecko.com/api/v3/coins/bitcoin');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching crypto details:', error);
+        return null;
+    }
+}
+
+export const getExchanges = async ()=> {
+try {
+const options = {
+  method: 'GET',
+  url: 'https://coingecko.p.rapidapi.com/exchanges',
+  headers: {
+    'x-rapidapi-key': '3881f835b7mshcdc185d253e66b4p1381b6jsn94eb41fcf016',
+    'x-rapidapi-host': 'coingecko.p.rapidapi.com'
+  }
+};
+
+	const response = await axios.request(options);
+    return response.data;
+} catch (error) {
+	console.error(error);
+    return null;
+}
+}
+export const getCryptoCoinDetails = async (uuid) => {
+
+const options = {
+  method: 'GET',
+  url: `https://coinranking1.p.rapidapi.com/coin/${uuid}`,
+  params: {
+    referenceCurrencyUuid: 'yhjMzLPhuIDl',
+    timePeriod: '24h',
+    // uuid: uuid
+  },
+  headers: {
+    'x-rapidapi-key': '3881f835b7mshcdc185d253e66b4p1381b6jsn94eb41fcf016',
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	// console.log(response.data);
+    return response.data;
+} catch (error) {
+	console.error(error);
+    return null
+}
+}
+
+export const getCoinList = async () => {
+  const options = {
+    method: 'GET',
+    url: 'https://coingecko.p.rapidapi.com/coins/list',
+    headers: {
+      'x-rapidapi-key': '3881f835b7mshcdc185d253e66b4p1381b6jsn94eb41fcf016',
+      'x-rapidapi-host': 'coingecko.p.rapidapi.com'
+    }
+  };
+  try {
+    const response = await axios.request(options);
+    return response.data
+  } catch (error) {
+    console.error(error);
+  }
+}
+export const getCryptoCoinData = async(coinId)=> {
+  const options = {
+    method: 'GET',
+    url: `https://coingecko.p.rapidapi.com/coins/${coinId}`,
+    params: {
+      localization: 'true',
+      tickers: 'true',
+      market_data: 'true',
+      community_data: 'true',
+      developer_data: 'true',
+      sparkline: 'false'
+    },
+    headers: {
+      'x-rapidapi-key': '3881f835b7mshcdc185d253e66b4p1381b6jsn94eb41fcf016',
+      'x-rapidapi-host': 'coingecko.p.rapidapi.com'
+    }
+  };
+  
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
